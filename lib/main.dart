e380f9ea8225922a4e81c34bd9bec92b1d759b98
ohFRY <akerzz@gmail.com>
2025-02-01 00:31:47 +0800

    Add Supabase dependency and initialize in main function; register new plugins for app links and URL launcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:lightseed/src/ui/screens/splash_screen.dart';
 import 'package:lightseed/src/ui/app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:lightseed/config.dart'; // Import the configuration file
 
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
+
   runApp(MyAppWithSplashScreen());
 }
 
