813ad39866f36a8e28a5f9862235ff704449138c
ohFRY <akerzz@gmail.com>
2025-02-08 01:52:45 +0800

    Enhance Sign Screen UI with improved layout and increased text size for better accessibility

--- lib/src/ui/screens/sign_screen.dart
+++ lib/src/ui/screens/sign_screen.dart
@@ -20,60 +20,90 @@ class SignScreenState extends State<SignScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text(isSignUp ? 'Sign Up' : 'Login'),
+          centerTitle: true,
           automaticallyImplyLeading: false,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(labelText: 'Email'),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 32.0),
+              child: Image.asset(
+                'assets/logo.png',
+                height: 100, // Adjust the height as needed
               ),
-              TextField(
-                controller: passwordController,
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
+            ),
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(labelText: 'Email'),
+                        style: TextStyle(fontSize: 18), // Increased text size
+                      ),
+                      TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(labelText: 'Password'),
+                        obscureText: true,
+                        style: TextStyle(fontSize: 18), // Increased text size
+                      ),
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () async {
+                          final email = emailController.text;
+                          final password = passwordController.text;
+                          String? response;
+                          if (isSignUp) {
+                            response = await AuthLogic.signUp(email, password);
+                          } else {
+                            response = await AuthLogic.signIn(email, password);
+                          }
+
+                          if (response != null) {
+                            if (context.mounted) {
+                              context.showSnackBar(response, isError: true);
+                            }
+                          }
+                          if (context.mounted) {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(builder: (context) => MyApp()),
+                            );
+                          }
+                        },
+                        child: Text(isSignUp ? 'Sign Up' : 'Login'),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () async {
-                  final email = emailController.text;
-                  final password = passwordController.text;
-                  String? response;
-                  if (isSignUp) {
-                    response = await AuthLogic.signUp(email, password);
-                  } else {
-                    response = await AuthLogic.signIn(email, password);
-                  }
-                  
-                  if (response != null) {
-                    if (context.mounted) {context.showSnackBar(response, isError: true);}
-                  }
-                  if (context.mounted){
-                     Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => MyApp()),
-                  ); 
-                  }
-                },
-                child: Text(isSignUp ? 'Sign Up' : 'Login'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Column(
+                children: [
+                  ElevatedButton(
+                        onPressed: () async {
+                          await AuthLogic.signInWithGoogle();
+                        },
+                        child: Text('Sign In with Google'),
+                      ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isSignUp = !isSignUp;
+                      });
+                    },
+                    child: Text(isSignUp
+                        ? 'Already have an account? Login'
+                        : 'Don\'t have an account? Sign Up'),
+                  ),
+                ],
               ),
-              ElevatedButton(
-                onPressed: () async {
-                  await AuthLogic.signInWithGoogle();
-                },
-                child: Text('Sign In with Google'),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    isSignUp = !isSignUp;
-                  });
-                },
-                child: Text(isSignUp ? 'Already have an account? Login' : 'Don\'t have an account? Sign Up'),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
